975ae3d407944ba5ae0ae4f8132f0a0ba2c6db99
AlexandreSCorreia <alex.sol.correia@gmail.com>
2022-03-24 00:46:21 -0300

    subindo projeto

---
+++ src/gui/MainViewController.java
@@ -0,0 +1,87 @@
+ package gui;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.function.Consumer;
+
+import application.Program;
+import gui.util.Alerts;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
+import model.services.DepartmentService;
+import model.services.SellerService;
+
+public class MainViewController implements Initializable {
+
+	@FXML
+	private MenuItem menuItemSeller;
+
+	@FXML
+	private MenuItem menuItemDepartment;
+
+	@FXML
+	private MenuItem menuItemAbout;
+
+	@FXML
+	public void onMenuItemSellerAction() {
+		loadView( "/gui/SellerList.fxml",(SellerListController controller) ->{
+			controller.setSellerService(new SellerService());
+			controller.updateTableView();
+		});
+	}
+
+	@FXML
+	public void onMenuItemDepartmentAction() {
+		loadView( "/gui/DepartmentList.fxml",(DepartmentListController controller) ->{
+			controller.setDepartmentService(new DepartmentService());
+			controller.updateTableView();
+		});
+	}
+
+	@FXML
+	public void onMenuItemAboutAction() {
+		loadView( "/gui/AboutView.fxml", x ->{});
+	}
+
+	private synchronized <T> void loadView(String absoluteName,Consumer<T> initializingAction) {
+		try {
+			
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			VBox newVBox = loader.load();
+			
+			Scene mainScene = Program.getScene();
+			//Pegando uma referencipara o VBOX dentro do SCROLLPANE
+			VBox mainVBox = (VBox)((ScrollPane) mainScene.getRoot()).getContent(); 
+			//Capturando o menu para manter ele
+			Node mainMenu = mainVBox.getChildren().get(0);
+			//Limpando o vbox
+			mainVBox.getChildren().clear();
+			//adicionando o menu
+			mainVBox.getChildren().add(mainMenu);
+			//adicionando uma coleção que será os filhos do newVBox
+			mainVBox.getChildren().addAll(newVBox.getChildren());
+			
+			T controller = loader.getController();
+			initializingAction.accept(controller);
+			
+		} catch (IOException e) {
+			Alerts.showAlert("IO Exception", "Error loading view ", e.getMessage(), AlertType.ERROR);
+		}
+	}
+	
+
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
